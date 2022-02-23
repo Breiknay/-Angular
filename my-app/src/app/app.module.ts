03e0695261b0e37ca35888a5d41303e42ddb4841
amaldavanau <artsiom.moldovanov@bbv.by>
2022-02-23 09:28:48 +0300

    [FIX|UPDATE] Style adjustments: * fix: mat-card and button style; * update: added toolbar on pages.

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,29 +1,28 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from "@angular/forms";
-import { ModelComponent } from './model/model.component';
-import { ModelInStockComponent } from './model-in-stock/model-in-stock.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { Routes,Router, RouterModule } from "@angular/router";
-import { BuylogComponent } from './buylog/buylog.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ModelComponent} from './model/model.component';
+import {ModelInStockComponent} from './model-in-stock/model-in-stock.component';
+import {ContactsComponent} from './contacts/contacts.component';
+import {Routes, Router, RouterModule} from "@angular/router";
+import {BuylogComponent} from './buylog/buylog.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from "@angular/material/slider";
 import {MatCardModule} from "@angular/material/card";
 import {MatTabsModule} from "@angular/material/tabs";
 import {MatDividerModule} from '@angular/material/divider'
-import {MatButtonModule} from '@angular/material/button'
 import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 const appRoutes: Routes = [
-  {path :'',component: ModelComponent} ,
-{path : 'ModelInStock' ,component : ModelInStockComponent},
-  {path : 'Contacts' ,component : ContactsComponent},
-  {path : '**' ,component : BuylogComponent},
-  ];
+  {path: '', component: ModelComponent},
+  {path: 'ModelInStock', component: ModelInStockComponent},
+  {path: 'Contacts', component: ContactsComponent},
+  {path: '**', component: BuylogComponent},
+];
 
 @NgModule({
   declarations: [
@@ -44,9 +43,12 @@ const appRoutes: Routes = [
     MatTabsModule,
     MatDividerModule,
     MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- my-app/src/app/model/model.component.ts
+++ my-app/src/app/model/model.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
-
-
 
 
 @Component({
@@ -12,38 +10,38 @@ import {Router} from "@angular/router";
 })
 
 
-
 export class ModelComponent implements OnInit {
   nameCarGle = 'Gle Coupe'
   carImgGle = "https://mbbelarus.by/upload/resize_cache/iblock/7bc/438_274_1/x3xfjug02e5db47o4h9ieb73fy9fui7g1.png.pagespeed.ic.RngUCd8Wk-.webp"
 
-      nameCarE = 'E - class'
-      carImgE = "https://mbbelarus.by/upload/resize_cache/iblock/846/438_274_1/xtt7q517mfuvdprai6kr7skvgidcztl5l.png.pagespeed.ic.sM5yKLalBy.webp"
-      nameCarS='S - class'
-      carImgS = "https://mbbelarus.by/upload/resize_cache/iblock/e49/438_274_1/xi3794bgnfgvyeenedro1s7gmfr1tkv80.png.pagespeed.ic.W2PtmxIe41.webp"
+  nameCarE = 'E - class'
+  carImgE = "https://mbbelarus.by/upload/resize_cache/iblock/846/438_274_1/xtt7q517mfuvdprai6kr7skvgidcztl5l.png.pagespeed.ic.sM5yKLalBy.webp"
+  nameCarS = 'S - class'
+  carImgS = "https://mbbelarus.by/upload/resize_cache/iblock/e49/438_274_1/xi3794bgnfgvyeenedro1s7gmfr1tkv80.png.pagespeed.ic.W2PtmxIe41.webp"
 
-      nameCarGls = 'Gls'
-      carImgGls ='https://mbbelarus.by/upload/resize_cache/iblock/4c7/438_274_1/x8obzbfw94parsec4j199pqmpfo1sq044.png.pagespeed.ic.Kk3cCV_f_b.webp'
+  nameCarGls = 'Gls'
+  carImgGls = 'https://mbbelarus.by/upload/resize_cache/iblock/4c7/438_274_1/x8obzbfw94parsec4j199pqmpfo1sq044.png.pagespeed.ic.Kk3cCV_f_b.webp'
 
-      nameCarC ='C - class'
-      carImgC = "https://mbbelarus.by/upload/resize_cache/iblock/55e/438_274_1/xmfxpaypi32iye41ucixcdkr6q8j47gzv.png.pagespeed.ic.OSa3PLjHKF.webp"
-      nameCarCla = 'CLA- class'
-      carImgCla = 'https://mbbelarus.by/upload/resize_cache/iblock/ff1/438_274_1/x5lt071nmihyntvvt7aq2j7vay1bvolfq.png.pagespeed.ic.MwFS-D-WQM.webp'
+  nameCarC = 'C - class'
+  carImgC = "https://mbbelarus.by/upload/resize_cache/iblock/55e/438_274_1/xmfxpaypi32iye41ucixcdkr6q8j47gzv.png.pagespeed.ic.OSa3PLjHKF.webp"
+  nameCarCla = 'CLA- class'
+  carImgCla = 'https://mbbelarus.by/upload/resize_cache/iblock/ff1/438_274_1/x5lt071nmihyntvvt7aq2j7vay1bvolfq.png.pagespeed.ic.MwFS-D-WQM.webp'
 
-      nameCarG = 'G- class'
-      carImgG = 'https://mbbelarus.by/upload/resize_cache/iblock/879/438_274_1/x00qqcwajtc61z3itq1py71zflxyz7i1x.png.pagespeed.ic.vSChQzqLfH.webp'
+  nameCarG = 'G- class'
+  carImgG = 'https://mbbelarus.by/upload/resize_cache/iblock/879/438_274_1/x00qqcwajtc61z3itq1py71zflxyz7i1x.png.pagespeed.ic.vSChQzqLfH.webp'
 
-    nameCarV = 'V-class'
-      carImgV = 'https://mbbelarus.by/upload/resize_cache/iblock/498/438_274_1/xafz3817lpendglyygvyod0m6p2oe8px8.png.pagespeed.ic.n7URZbOjdK.webp'
+  nameCarV = 'V-class'
+  carImgV = 'https://mbbelarus.by/upload/resize_cache/iblock/498/438_274_1/xafz3817lpendglyygvyod0m6p2oe8px8.png.pagespeed.ic.n7URZbOjdK.webp'
 
   longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
   from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
   originally bred for hunting.`;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+  }
 
 
-  myEvent($event: MouseEvent){
+  myEvent($event: MouseEvent) {
     this.router.navigate(['/buylog']);
   }
 
